c46d2ef75d7bdb460d2f9a5953aadaae7eb4e809
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 18:18:11 +0900

    Update stock.py

--- data/stock.py
+++ data/stock.py
@@ -2,6 +2,7 @@
 
 from config import NOTION_TOKEN, NOTION_PRICE_DB_ID, NOTION_TRADE_DB_ID
 from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
+from utils.logger import logger
 import yfinance as yf      # Yahoo 증권에서 데이터 받아오기
 import requests            # 네이버 증권에서 데이터 받아오기
 
