379bd72e2b8a148e8865343f53f0e690d9f9c759
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 18:04:39 +0900

    Update recommendation.py

--- analysis/recommendation.py
+++ analysis/recommendation.py
@@ -1,3 +1,4 @@
 # 리밸런싱 결과를 토대로 사용자에게 전달할 피드백 생성
 
-from config import NOTION_TOKEN 
+from config import NOTION_TOKEN, NOTION_PRICE_DB_ID, NOTION_TRADE_DB_ID
+from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
