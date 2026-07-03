baf63f0e758c3b5012a3f8a07c759465750b502d
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 19:31:46 +0900

    Update main.py

--- domestic_stock/main.py
+++ domestic_stock/main.py
@@ -6,3 +6,10 @@ from domestic_stock.read import get_ticker
 for page in get_all_pages(NOTION_PRICE_DB_ID):
 
   ticker = get_ticker(page)
+  
+  stock_info = {
+      **get_naver_prop(ticker),
+      **get_yfinance_prop(ticker)
+  }
+
+  
