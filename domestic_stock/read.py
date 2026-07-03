407c68e238a381388b901019254f56d5a9d10873
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-03 19:30:15 +0900

    Update read.py

--- domestic_stock/read.py
+++ domestic_stock/read.py
@@ -1,7 +1,3 @@
-from config import NOTION_PRICE_DB_ID
-from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
-from notion.get_all_pages import get_all_pages()
-
 def get_ticker(page):
     ticker_data = page["properties"]["티커"]["rich_text"]
 
