2981a39fe4ef9c894f11c7a953f0cdcbae4b1ca9
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-05 15:50:49 +0900

    Update update.py

--- gold/update.py
+++ gold/update.py
@@ -3,7 +3,7 @@
 from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
 from notion.rich_text import rich_text
 from utils.logger import logging
-from config import 
+from config import NOTION_KRX_GOLD_INFO_DB_ID
 
 def update_KRX_GOLD_INFO_DB(gold_info):
 
@@ -31,6 +31,6 @@ def update_KRX_GOLD_INFO_DB(gold_info):
   } 
     
   notion.pages.update(
-      page_id=page["id"],
+      page_id=NOTION_KRX_GOLD_INFO_DB_ID,
       properties=gold_info_mod
   )
