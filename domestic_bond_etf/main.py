978825f9f5a2f13dcc560bf19c135ca9dfe09082
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-09 20:34:37 +0900

    Update main.py

--- domestic_bond_etf/main.py
+++ domestic_bond_etf/main.py
@@ -1,1 +1,16 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from domestic_bond_etf.read import get_ticker
+from domestic_bond_etf.update import update_bond_etf_DB
+from data.domestic_bond_etf import get_naver_prop
+from history.history import append_prices
 
+def domestic_bond_etf_DB_main(pages):
+    for page in pages:
+        ticker = get_ticker(page)
+        if not ticker:
+            continue
+
+        stock_info = get_naver_prop(ticker)
+
+        update_bond_etf_DB(page, stock_info)
